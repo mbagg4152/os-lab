b4269a20cde68022004bc5cbc8e1a6d4768dfb8a
Maggie <50422221+mbagg4152@users.noreply.github.com>
2020-07-25 15:30:19 -0400

    Update mysort.c

--- lab1/mysort.c
+++ lab1/mysort.c
@@ -10,7 +10,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void sort(int nums[], int len);
+void sort(int nums[], int len); // remove this n then move main to the end or smth
 
 int main(int argc, char** argv){    
     if (argc == 1) { // quit if no args supplied
@@ -18,28 +18,31 @@ int main(int argc, char** argv){
         return 0;
     }
 
-    int stored, temp;
+    int stored, temp; // dont need stored, prob keep temp but rename 
     int cap = 10; // intial array capacity
     int max = 1000; // max number of vals to read in
     int* nums = (int*) malloc(sizeof(int)*cap); // array to hold numbers from file
     FILE* file = fopen(argv[1], "r"); // read number file 
 
+    
+    // dont need to do this fancy realloc shit, max num of nums we need to read in is 1000 so just make beeg array
     while ((!feof(file)) && (stored < max)){ // read number file and populate dynamically sized array.
         fscanf(file, "%d", &temp);
         if (stored == cap) { // change size of array 
-            cap = cap *2;
-            nums = (int*) realloc(nums, sizeof(int)*(cap));
+            cap = cap *2; // no
+            nums = (int*) realloc(nums, sizeof(int)*(cap)); // no
         }
         nums[stored] = temp; // put number in array
-        stored++;
+        stored++; // probably dont need this bc tfw no realloc
     }
     
     printf("the numbers from the file, sorted:\n\n");
     sort(nums, stored); // sort and print num array
-    free(nums); 
+    free(nums); // ig this could stay but w/e its the end of the stuff
     return 0;
 }
 
+// use a different sorting algo pls n thank, there r many, hard 2 go wrong
 void sort(int nums[], int len){ // implementation of insertion sort
     int this, prev;
     for (int i = 0; i < len; i++){
@@ -54,6 +57,7 @@ void sort(int nums[], int len){ // implementation of insertion sort
         nums[prev + 1] = this;
     }
 
+    // just print the binches 1 per line, also if not using cool array then make sure to do a NULL check or smth so you dont make the computer stroke out
     int fcounter = 0; // counter stored for output formatting    
     for (int i = 0; i < len; i++){
         if (i == (len - 1)) printf("%d\n", nums[i]);
