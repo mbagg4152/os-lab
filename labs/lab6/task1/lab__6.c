88959065c955ae08c71b39e7b87c77bb45be6917
mag <mag@buntu>
2020-08-30 10:39:13 -0400

    Update lab6, lab__6.c, and 3 more files...

--- labs/lab6/task1/lab__6.c
+++ labs/lab6/task1/lab__6.c
@@ -1,7 +1,8 @@
 /*************************************************************************************************************
  * Maggie Horton
  * CS-451L Summer 2020
- * lab__6.c, task 2:
+ * lab__6.c, task 1: added threads to generate numbers and used mutex lock & unlock in the critical section,
+ * which was the part of the code modifying the sum variable.
 *************************************************************************************************************/
 
 #include <stdlib.h>     //NULL
@@ -17,12 +18,8 @@
 long sum;                   // Sum of generated values
 int finished_producers;     // number of the producer that finished producing
 
-
 //C: Mutex declaration and initialization
 static pthread_mutex_t gen_mutex = PTHREAD_MUTEX_INITIALIZER;
-
-//F: Condition variable declaration and initialization
-
 
 int main(void) {
 
@@ -31,36 +28,29 @@ int main(void) {
     sum = 0;
 
     //A: Create five generator threads
-    pthread_t gen_thread1, gen_thread2, gen_thread3, gen_thread4, gen_thread5;
-    pthread_create(&gen_thread1, NULL, generator_function, NULL);
-    pthread_create(&gen_thread2, NULL, generator_function, NULL);
-    pthread_create(&gen_thread3, NULL, generator_function, NULL);
-    pthread_create(&gen_thread4, NULL, generator_function, NULL);
-    pthread_create(&gen_thread5, NULL, generator_function, NULL);
-
-    //D: Create print thread
-    pthread_t print_thread;
-    pthread_create(&print_thread, NULL, print_function, NULL);
+    pthread_t gen1, gen2, gen3, gen4, gen5;
+    pthread_create(&gen1, NULL, generator_function, NULL);
+    pthread_create(&gen2, NULL, generator_function, NULL);
+    pthread_create(&gen3, NULL, generator_function, NULL);
+    pthread_create(&gen4, NULL, generator_function, NULL);
+    pthread_create(&gen5, NULL, generator_function, NULL);
 
     //B: Makes sure that all generator threads has finished before proceeding
-    pthread_join(gen_thread1, NULL);
-    pthread_join(gen_thread2, NULL);
-    pthread_join(gen_thread3, NULL);
-    pthread_join(gen_thread4, NULL);
-    pthread_join(gen_thread5, NULL);
+    pthread_join(gen1, NULL);
+    pthread_join(gen2, NULL);
+    pthread_join(gen3, NULL);
+    pthread_join(gen4, NULL);
+    pthread_join(gen5, NULL);
 
+    print_function();
 
-    //E: Makes sure that print thread has finished before proceeding
-    pthread_join(print_thread, NULL);
     return (0);
 
 }
 
 void *generator_function(void *v_param) {
-
     int counter = 0;
     long this_sum = 0;
-
 
     while (counter < 200L) {
         pthread_mutex_lock(&gen_mutex);
@@ -78,24 +68,20 @@ void *generator_function(void *v_param) {
 
     printf("\033[1;31m"); // red
     printf("---+---+----+----------+---------+---+--+---+------+------+---+--+---\n");
-    printf("The sum of produced items by thread %ld at the end is: %ld \n", pthread_self(), this_sum);
+    printf("The sum of produced items by thread %ld at the end is: %ld \n", pthread_self(),
+           this_sum);
     printf("---+---+----+----------+---------+---+--+---+------+------+---+--+---\n\n");
-    printf("\033[0m"); // reset color
+    printf("\033[0m");
 
     finished_producers++;
-
-    //H: If all generator has finished fire signal for condition variable
 
     return (0);
 }
 
 void *print_function(void) {
-
-    //G: Wait until all generator has finished
-
     printf("\033[1;36m"); // cyan
     printf("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     printf("The value of counter at the end is: %ld \n", sum);
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-    printf("\033[0m"); // reset color
+    printf("\033[0m");
 }
