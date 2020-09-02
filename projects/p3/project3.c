9644521d2a8d3b1d220ea1cd28aa3c73b8a6ba91
mag <mag@buntu>
2020-09-01 21:51:49 -0400

    Update Makefile, project3, and 3 more files...

--- projects/p3/project3.c
+++ projects/p3/project3.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int people_count;
+int max_wander;
+int floor_count;
+
 int main(void)
 {
     return 0;
--- projects/p3/project3.h
+++ projects/p3/project3.h
@@ -1,4 +1,9 @@
-#define OPEN_HOURS  9               // from 8am - 5pm
-#define MIN_WANDER  1               // people must wander for at least 1 sec
-#define MAX_WANDER  32400           // number of seconds for the 'open' hours
-#define BASE        0               // people start and end at floor 0
+#define BASE                0       // people start and end at floor 0
+#define DEFAULT_FLOORS      10      // default number of floors
+#define DEFAULT_PEOPLE      1       // default number of people
+#define DEFAULT_TIME        10      // default wandering time
+#define MIN_WANDER          1       // people must wander for at least 1 sec
+#define OPEN_HOURS          9       // from 8am - 5pm
+#define TACK_F              "-f "
+#define TACK_P              "-p "
+#define TACK_W              "-w "
